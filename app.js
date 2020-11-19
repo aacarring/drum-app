fd62e7c7d3c0d29f8e41d7cfbecfeb524a731414
Alexa Carrington <acarring@wellesley.edu>
2020-11-19 13:10:51 -0500

    Linked Tone in html, created functions in js

---
+++ app.js
@@ -0,0 +1,23 @@
+function sequencer() {
+    const kick = new Tone.Player('/Samples/kick-electro01.wav').toDestination();
+    const snare = new Tone.Player('/Samples/snare-lofi02.wav').toDestination();
+    let index = 0; 
+
+    Tone.Transport.scheduleRepeat(repeat, '8n');
+    Tone.Transport.start();
+
+    function repeat() {
+        let step = index % 8;
+        let kickInputs = document.querySelector(`.kick input:nth-child(${step + 1})`);
+        let snareInputs = document.querySelector(`.snare input:nth-child(${step + 1})`);
+        if (kickInputs.checked) {
+            kick.start();
+        }
+        if (snareInputs.checked) {
+            snare.start();
+        }
+        index++;
+    }
+}
+
+sequencer();
